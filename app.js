bb86db6508c1484d67049eb13f3798258ce2c5ef
ovo <changyewei.com.gmail.com>
2023-11-02 07:56:47 +0800

    绘制彩色三角形

--- app.js
+++ app.js
@@ -7,27 +7,32 @@ let gl = canvas.getContext('webgl')
 //3D
 let vertexShader = `
 attribute vec2 a_position;
+attribute vec3 a_color;
+varying vec3 v_color;
 void main() {
+  v_color = a_color;
   gl_Position = vec4(a_position, 0.0, 1.0);
 }
 `
 let fragmentShader = `
+precision mediump float;
+varying vec3 v_color;
 
  void main() {
-  gl_FragColor = vec4(1.0,1.0,0.0, 1.0);
+  gl_FragColor = vec4(v_color, 1.0);
  }
 `
 
 initShaders(gl, vertexShader, fragmentShader)
 
 let vertices = [
-  -0.5, 0.0,
-  0.5, 0.0,
-  0.0, 0.8
+  -0.5, 0.0, 1.0, 0.0, 0.0,
+  0.5, 0.0, 0.0, 1.0, 0.0,
+  0.0, 0.8, 0.0, 0.0, 1.0
 ]
 
 vertices = new Float32Array(vertices)
-
+let FSIZE = vertices.BYTES_PER_ELEMENT
 
 //清空canvas画布
 gl.clearColor(0.5, 0.5, 0.5, 1.0)
@@ -38,15 +43,18 @@ let buffer = gl.createBuffer()
 
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
 
-gl.bufferData(gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW)
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
 
 let a_position = gl.getAttribLocation(gl.program, 'a_position')
-gl.vertexAttribPointer(
-  a_position, 2, gl.FLOAT, false, 0, 0)
+gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 5*FSIZE, 0)
 
-  gl.enableVertexAttribArray(a_position)
+let a_color = gl.getAttribLocation(gl.program, 'a_color')
+gl.vertexAttribPointer(a_color, 3, gl.FLOAT, false, 5*FSIZE, 2*FSIZE)
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+gl.enableVertexAttribArray(a_position)
+gl.enableVertexAttribArray(a_color)
+
+gl.drawArrays(gl.TRIANGLES, 0, 3)
 
 
 
